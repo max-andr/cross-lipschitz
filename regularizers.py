0e9a73bcd67b10a86381837df8b024ebf85acc28
Shoaib Ahmed Siddiqui <12bscsssiddiqui@seecs.edu.pk>
2018-04-08 16:53:16 +0500

    Added base code for new regularizer

--- regularizers.py
+++ regularizers.py
@@ -26,6 +26,40 @@ def cross_lipschitz(f, x, n_ex, hps):
             reg += 2 * tf.reduce_sum(tf.square(norm_for_batch))  # 2 comes from the fact, that we do summation only for distinct pairs (l, m)
     return reg / n_summations
 
+def cross_lipschitz_updated(f, x, y, n_ex, hps):
+    """
+    Calculates Cross-Lipschitz regularization in a straightforward way using tf.gradients to calculate the required
+    derivatives. Suitable for all differentiable classifiers. It is calculated for the given batch (details:
+    "Formal Guarantees on the Robustness of a Classifier against Adversarial Manipulation",
+    http://www.ml.uni-saarland.de/Publications/HeiAnd-FormGuarAdvManipLongVersion.pdf).
+
+    f: tensor, (batch_size, n_classes) - values of the output layer before softmax for a batch
+    x: tensor, (batch_size, n_input) or (batch_size, height, width, color) - input images
+    y: tensor, (batch_size) - input image labels
+    n_classes: int - number of classes
+    n_ex: int - number of examples in a batch
+    """
+    n_summations = tf.cast(hps.n_classes ** 2 * n_ex, tf.float32)  # normalizing factor to unify scale of lambda across datasets
+    reg = 0
+    grad_matrix_list = [tf.gradients(f[:, k], x)[0] for k in range(hps.n_classes)]  # take each gradient wrt input only once
+    if hps.as_image:  # if x has shape (batch_size, height, width, color), then we need to flatten it first
+        grad_matrix_list = [tf.reshape(grad, [-1, hps.real_height * hps.real_width * hps.n_colors]) for grad in grad_matrix_list]
+
+    for inputIter in range(len(y)):
+        maxClsGradIdx = 0
+        for m in range(1, hps.n_classes):
+            if tf.norm(grad_matrix_list[m], ord=2, axis=1) > tf.norm(grad_matrix_list[maxClsGradIdx], ord=2, axis=1):
+                maxClsGradIdx = m
+
+        grad_diff_matrix = grad_matrix_list[l] - grad_matrix_list[maxClsGradIdx]  # difference of gradients for a class pair (l, m)
+        norm_for_batch = tf.norm(grad_diff_matrix, ord=2, axis=1)
+
+        grad_diff_matrix = grad_matrix_list[l] - grad_matrix_list[maxClsGradIdx]  # difference of gradients for a class pair (l, m)
+        norm_for_batch = tf.norm(grad_diff_matrix, ord=2, axis=1)
+
+        reg += 2 * tf.reduce_sum(tf.square(norm_for_batch))  # 2 comes from the fact, that we do summation only for distinct pairs (l, m)
+    return reg / n_summations
+
 
 def cross_lipschitz_analytical_1hl(model, n_ex, hps):
     """
